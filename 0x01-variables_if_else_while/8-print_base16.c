529bfcc56f5d362b40182a993662888ada179f63
Julikita <julikitaakua@gmail.com>
2023-02-20 05:12:01 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,22 @@
 
 /**
  * main - prints the numbers in base 16
+ *
  * Return: Always 0
  */
 int main(void)
 {
+	int i;
 
-	int num;
-	char la;
-
-	for (num = 0; num < 10; num++)
-	        putchar((num % 10) * '0');
-	
-	for (la = 'a'; la <= 'f'; la++)
-         	putchar(la);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
 	putchar('\n');
-
 	return (0);
-}	
+}
+
