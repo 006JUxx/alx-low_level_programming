59e406bb650005e1e3e8a6d119cc0c1d83f01992
Julikita <julikitaakua@gmail.com>
2023-02-20 06:17:24 -0800

    The success combination in business is:Do what you do better...and:do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
-#include <stdlid.h>
 
 /**
- * main - prints all possible combination of three digits
- * Return: Always 0
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c;
-	int d;
-	int o =0;
-	 while(o < 10)
-	 {	
-		 d=0;
-                 while (d < 10)
-		 {
-			 c = 0;
-			 while (c < 10)
-			 {
-				 if (c != d && d != o && o < d < c)
-				 {
-					 putchar('0 + o);
-					 putchar('0 + d);
-					 putchar('0 + C);
-					 if (c + d + o != 0 + 0 + 3)
-					 {
-						 putchar(',')
-						 putchar(' ') 
-					 }
-				 }
-				 c++;
-			 }
-			 d++;
-		 }
-		 o++;
-	 }
-		 putchar('\n');
-		 return(0);
-}
+	int n, m, l;
 
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',')
+						putchar(' ')
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
