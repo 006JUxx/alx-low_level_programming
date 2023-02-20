2336f49a0eae89af5d938f7c970ee4b380f7eb60
Julikita <julikitaakua@gmail.com>
2023-02-20 10:41:54 -0800

    The success combination in business is:Do what you do better...and:do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,51 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of three digits
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n, m, l;
+	int digit1;
+	int digit2;
+	int digit3;
 
-	for (n = 48; n < 58; n++)
+	digit1 = 48;
+	digit2 = 49;
+	digit = 50;
+	while ((digit1 < 56) && (digit2 < 57) && (digit3 < 58))
 	{
-		for (m = 49; m < 58; m++)
+		putchar(digit1);
+		putchar(digit2);
+		putchar(digit3);
+		if ((digit1 == 56) && (digit2 == 56) && (digit3 == 57))
 		{
-			for (l = 50; l < 58; l++)
+			putchar('\n');
+			digit1++;
+			digit2++;
+			digit3++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (digit3 < 57)
 			{
-				if (l > m && m > n)
-				{
-					putchar(n);
-					putchar(m);
-					putchar(l);
-					if (n != 55 || m != 56)
-					{
-						putchar(',')
-						putchar(' ')
-					}
-				}
+				digit3++;
+			}
+			else if ((digit2 < 56) && (digit3 == 57))
+			{
+				digit3 = 1 + ++digit2;
+			}
+			else
+			{
+				digit2 = 1 + ++digit1;
+				digit3 = 1 + digit2;
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
-}
